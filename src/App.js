946c88a75804c7220f3f54f9f416b14f8f6a7d46
unknown <gargpoooja192@gmail.com>
2024-04-21 04:55:55 +0200

    Navbar construction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  
+} from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Project";
+import Resume from "./components/Resume/Resume";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+import NavBar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/about" element={<About />}  />
+          <Route path="/projects" element={<Projects />}  />
+          <Route path="/resume" element={<Resume />}  />
+
+        </Routes>
+      </div>
+    </Router>
+   
   );
 }
 
